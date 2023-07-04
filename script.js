b7da1bc0dde3ae6ceea1d2c699f5bb67c75b0a61
Adebukol <adebukolaomowole@gmail.com>
2023-07-04 16:03:14 +0100

    api set

---
+++ script.js
@@ -0,0 +1,6 @@
+const apiKey= "c3b6561062177fc3c7f41810ed2dfc63";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Osogbo";
+
+async function checkWeather(){
+  const response = await fetch()
+}
